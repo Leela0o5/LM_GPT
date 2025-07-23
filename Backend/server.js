2f4b195c63fba4f893b55850d43710dfa923b902
Leela0o5 <leela592023@gmail.com>
2025-07-23 22:30:01 +0530

    Setup Utils, Connection with DB and test Route

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,30 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import { Messages } from "openai/resources/chat/completions.js";
+import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 app.use(cors());
-
+app.use("/api", chatRoutes);
 app.listen(PORT, () => {
   console.log("app is listening.");
+  connectDB();
 });
 
-app.post("/test", async (req, res) => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected with database!");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+/* app.post("/test", async (req, res) => {
   const options = {
     method: "POST",
     headers: {
@@ -42,3 +53,4 @@ app.post("/test", async (req, res) => {
     console.log(err);
   }
 });
+ */
